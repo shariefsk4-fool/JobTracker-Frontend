07022c0656fe15d0df44b4e16ca6e7d745118c81
Sharief Sk <sharief.sk04@gmail.com>
2026-03-15 01:26:39 +0530

    added url on axios api

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL:"http://localhost:8081/api"
+  baseURL:"https://jobtracker-backend-1bql.onrender.com"
 });
 
 API.interceptors.request.use((req)=>{
